c08ff0e466db31b5cb5b5ac7a5c9acabc1800a45
Michael Elvers <melvers@gmail.com>
2018-11-17 21:55:21 +0100

    Added command line options

--- WhatsBookSharp/Program.cs
+++ WhatsBookSharp/Program.cs
@@ -1,32 +1,60 @@
 ﻿using System;
+using CommandLine;
 using System.IO;
-using System.Text;
-using System.Globalization;
-using System.Text.RegularExpressions;
-using MessageParser;
 using Helper;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace WhatsBookSharp
 {
     class MainClass
     {
+        public class Options
+        {
+            [Option('i', "inputdir", Required = true, HelpText = "Input directory, should contain a subdirectory 'chat' where conversion and images is stored")]
+            public string InputDir { get; set; }
+
+            [Option('o', "outputdir", Required = false, HelpText = "Output directory, default is input directory")]
+            public string OutputDir { get; set; }
+
+            [Option('e', "emojidir", Required = true, HelpText = "Directory where the emoji png images are stored")]
+            public string EmojiDir{ get; set; }
+
+            [Option("imagepooldir", Required = false, HelpText = "Directory of the image pool. Only used when 'media omitted' messages are found")]
+            public string ImagePoolDir{ get; set; }
+
+            [Option("debugdir", Required = false, HelpText = "Directory where debug information is stored")]
+            public string DebugDir{ get; set; }
+        }
+
         public static void Main(string[] args)
         {
-            var writer = new StreamWriter("/tmp/output.log");
-            EmojiParser.Debug = writer;
+            Options options = null;
+            Parser.Default.ParseArguments<Options>(args).WithParsed(o => options = o);
 
-            var dir = "/tmp/chat";
-            var imagePoolDir = "/tmp/imagepool";
-            var emojiDir = "/tmp/blub/emojis";
+            if (options == null)
+            {
+                return;
+            }
 
-            var creator = new BookCreator(dir, dir, emojiDir);
+            var inputDir = options.InputDir;
+            var outputDir = (options.OutputDir == null) ? inputDir : options.OutputDir;
+            var emojiDir = options.EmojiDir;
+            var imagePoolDir = options.ImagePoolDir;
+
+            var debugDir = options.DebugDir;
+            StreamWriter writer = null;
+            if (debugDir != null)
+            {
+                Directory.CreateDirectory(debugDir);
+                writer = new StreamWriter(Path.Combine(debugDir, "output.log"));
+                EmojiParser.Debug = writer;
+            }
+
+            var creator = new BookCreator(inputDir, outputDir, emojiDir);
             creator.ImagePoolDir = imagePoolDir;
             creator.WriteTex();
 
             EmojiParser.Debug = null;
-            writer.Close();
+            writer?.Close();
         }
     }
 }
